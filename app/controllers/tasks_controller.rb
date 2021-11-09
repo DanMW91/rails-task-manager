2b447f4ba8a137e68bd5f9d81510c5643c94da11
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-09 14:22:11 +0000

    form test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,30 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task.update(task_params)
+    raise
+    # puts params
+    # puts @task
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
